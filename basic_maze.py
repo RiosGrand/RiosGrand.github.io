f3f0423be341796d6d93c569a4cea59a58ca513f
RiosGrand <JRios@kippnyccp.org>
2015-05-21 13:01:37 -0400

    Update basic_maze.py

--- basic_maze.py
+++ basic_maze.py
@@ -3,8 +3,26 @@ import time
 import math
 
 set_spped(100)
+
 def move_foward(feet):
-  enc_tgt(
-def move_left(feet)
-def move right(feet)
-def move left(feet)
+  mm = 304.8*feet
+  steps = int(mm/11.34)
+  enc_tgt(1,1,steps)
+  time.sleep(.1)
+  fwd()
+  time.sleep(feet*2)
+  
+def move_left():
+  enc_tgt(0,1,15)
+  time.sleep(.1)
+  left()
+  time.sleep(2)
+  
+def move right():
+  enc_tgt(1,0,15)
+  time.sleep(.1)
+  right()
+  time.sleep(2)
+
+
+move_foward(4)
